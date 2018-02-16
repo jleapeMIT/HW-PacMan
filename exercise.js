68754cc754ffd35b0995e6caea14f339890d87a3
jleapeMIT <jleape@mit.edu>
2018-02-16 11:22:41 -0500

    In-class changes

--- exercise.js
+++ exercise.js
@@ -11,40 +11,41 @@ var exercise = {};
 exercise.flag = 0; // 0 = mouth open  1 = mouth shut
 exercise.increment = 20; // pixels to move either + or -
 exercise.run = function() {
-    exercise.updatePosition();
+
     exercise.checkWallCollision();
+    exercise.updatePosition();
     exercise.chooseImage();
 };
 exercise.updatePosition = function() {
-    exercise.pos.x += exercise.increment
-    exercise.img1.style.left = exercise.pos.x + 'px'
+    exercise.pos.x += exercise.increment;
+    exercise.img1.style.left = exercise.pos.x + 'px';
 };
 exercise.chooseImage = function() {
     // choose between all 4 images
     if (exercise.increment > 0) {
         if (exercise.flag === 1) {
-            exercise.img1.src = 'PacMan1.png'
-            exercise.flag = 0
+            exercise.img1.src = 'PacMan1.png';
+            exercise.flag = 0;
         } else {
-            exercise.img1.src = 'PacMan2.png'
-            exercise.flag = 1
+            exercise.img1.src = 'PacMan2.png';
+            exercise.flag = 1;
         }
     } else if (exercise.increment < 0) {
         if (exercise.flag === 1) {
-            exercise.img1.src = 'PacMan3.png'
-            exercise.flag = 0
+            exercise.img1.src = 'PacMan3.png';
+            exercise.flag = 0;
         } else {
-            exercise.img1.src = 'PacMan4.png'
-            exercise.flag = 1
+            exercise.img1.src = 'PacMan4.png';
+            exercise.flag = 1;
         }
     }
 };
 exercise.checkWallCollision = function() {
-    var max_x = 600 - exercise.img1.width
-    if (exercise.pos.x >= max_x) {
-        exercise.increment = - exercise.increment
-    } else if (exercise.pos.x <= 0) {
-        exercise.increment = - exercise.increment
+    var max_x = 800 - exercise.img1.width
+    if ((exercise.pos.x + exercise.increment) > max_x) {
+        exercise.increment *= -1;
+    } else if ((exercise.pos.x + exercise.increment) < 0) {
+        exercise.increment *= -1;
     }
 
 };
